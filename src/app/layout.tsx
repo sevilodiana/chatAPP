9e65e75f4897e1886970c8bc98209ed60a51070e
sevilodiana <135428308+sevilodiana@users.noreply.github.com>
2023-07-31 11:49:02 +0200

    updated favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,27 @@
-import Providers from '@/components/Providers'
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import Providers from "@/components/Providers";
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import Head from "next/head";
 
 export const metadata: Metadata = {
-  title: 'chatApp',
-  description: 'Message like you mean it!',
-}
+  title: "chatApp",
+  description: "Message like you mean it!",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body> 
-        <Providers>{children}</Providers>      
+      <Head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </Head>
+      <body>
+        <Providers>{children}</Providers>
       </body>
     </html>
-  )
+  );
 }
